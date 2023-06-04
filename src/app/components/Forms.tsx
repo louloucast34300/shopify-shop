ede03f69bc8a1a3e021519e40afffa83b4bb0574
LCastel <louis.castel34@icloud.com>
2023-06-04 19:40:58 +0200

    feat(query): add cart logic --wip2

--- src/app/components/Forms.tsx
+++ src/app/components/Forms.tsx
@@ -7,7 +7,7 @@ export default function Forms () {
     const [data, setData] = useState({price:"0", currency:"USD", file:null })
     console.log(data)
 
-    const handleSubmit = async (e) =>{
+    const handleSubmit = async (e:any) =>{
 
         e.preventDefault();
         const { price, currency } = data;
@@ -23,10 +23,10 @@ export default function Forms () {
                 <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
                 <span className="text-gray-500 sm:text-sm">$</span>
                 </div>
-                <input onChange={(e) => setData({...data, price:e.target.value})} type="text" name="price" id="price" className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="0.00"/>
+                <input onChange={(e:any) => setData({...data, price:e.target.value})} type="text" name="price" id="price" className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="0.00"/>
                 <div className="absolute inset-y-0 right-0 flex items-center">
                 <label htmlFor="currency" className="sr-only">Currency</label>
-                <select onChange={(e) => setData({...data, currency:e.target.value})}id="currency" name="currency" className="h-full rounded-md border-0 bg-transparent py-0 pl-2 pr-7 text-gray-500 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm">
+                <select onChange={(e:any) => setData({...data, currency:e.target.value})}id="currency" name="currency" className="h-full rounded-md border-0 bg-transparent py-0 pl-2 pr-7 text-gray-500 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm">
                     <option>USD</option>
                     <option>CAD</option>
                     <option>EUR</option>
@@ -40,7 +40,7 @@ export default function Forms () {
                 <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
                 <span className="text-gray-500 sm:text-sm">$</span>
                 </div>
-                <input onChange={(e) => setData({...data, file:e.target.files})} type="file" name="file" id="file" className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="0.00"/>
+                <input onChange={(e:any) => setData({...data, file:e.target.files})} type="file" name="file" id="file" className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="0.00"/>
             </div>
         </div>
         <div className="relative mt-2 rounded-md shadow-sm flex justify-center">
